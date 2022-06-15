88cd331f6288c7dd43027aa129f44ae05b50a6fb
Andrey Pisarevsky <pisarevskiy1977@gmail.com>
2022-06-15 20:04:31 +0300

    Update modelling.py

--- src/modelling.py
+++ src/modelling.py
@@ -1,43 +1,42 @@
 from data_prep import data_extract, data_prep
 
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 
-'''FITTING THE MODEL WITH TRAIN.CSV DATA'''
-def model_fitting():
-    X, preprocessor, y = data_prep('../HousePrices/src/train.csv')
-    X = preprocessor.fit_transform(X)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=50)
-
-    reg = RandomForestRegressor(max_depth=20)
-    
+'''FITTING THE MODEL WITH TRAIN.CSV DATA (RF, ADABOOST, GDBOOST, XGBOOST)'''
+def rf(X_train, X_test, y_train, y_test):
+    reg = RandomForestRegressor(n_estimators = 100, max_leaf_nodes=100)
     reg.fit(X_train, y_train)
     print(reg.score(X_test, y_test))
 
     return reg
 
-def model_evaluation():
-    pass
+def gdboost(X_train, X_test, y_train, y_test):
+    reg = GradientBoostingRegressor(n_estimators = 200, max_leaf_nodes=20, learning_rate=0.2)
+    reg.fit(X_train, y_train)
+    print(reg.score(X_test, y_test))
 
-def model_output():
+    return reg
+
+
+def model_application():
     X_train, preprocessor, y_train = data_prep('../HousePrices/src/train.csv')
     X_test, y_test = data_extract('../HousePrices/src/test.csv')
 
     X_train = preprocessor.fit_transform(X_train)
     X_test = preprocessor.fit_transform(X_test)
 
-    reg = RandomForestRegressor(max_depth=20)
-
-    reg.fit(X_train, y_train)
-    y_test = reg.predict(X_test)
-
     return y_test
 
 def main():
-    y_test = model_output()
+    X, preprocessor, y = data_prep('../HousePrices/src/train.csv')
+    X = preprocessor.fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-    print(y_test)
+    rf(X_train, X_test, y_train, y_test)
+    gdboost(X_train, X_test, y_train, y_test)
+
 main()
 
 # Next time continue hypertuning the RF regressor, visualize the error tendency based on the hyperparameters
